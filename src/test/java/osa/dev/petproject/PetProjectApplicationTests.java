637a5e52b35307f205d4d69b6442c9726446c498
Ovchinnikov Sergey <owchin.s@yandex.ru>
2022-02-23 14:43:26 +0300

    init commit

---
+++ src/test/java/osa/dev/petproject/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package osa.dev.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
