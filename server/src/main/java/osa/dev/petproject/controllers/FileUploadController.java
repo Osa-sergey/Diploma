06be6d4fee149b2f042965dba99698e5b7799350
sovchinnikov <owchin.s@yandex.ru>
2022-04-11 22:51:24 +0300

    Так загрузка файла работает в postman

---
+++ server/src/main/java/osa/dev/petproject/controllers/FileUploadController.java
@@ -0,0 +1,28 @@
+package osa.dev.petproject.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/files")
+public class FileUploadController {
+
+    @PostMapping
+    @RequestMapping(headers = ("content-type=multipart/*"), consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<String> handleFileUpload(@RequestParam(name = "file") MultipartFile file) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        List<String> input = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                                .lines()
+                                .collect(Collectors.toList());
+        return input;
+    }
+}
